b9348e5562148c645e63d103757bb0819806faa0
Tetsushi Omi <tetsu3491@gmail.com>
2022-02-16 17:43:06 -0500

    oh

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -7,7 +7,7 @@ use syn::{parse_macro_input, DeriveInput};
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    //eprintln!("{:#?}", ast);
+    //println!("{:#?}", ast);
     let name = ast.ident;
     //let bname = format!("{}Builder", name);
     //let bident = Ident::new(&bname, name.span());
