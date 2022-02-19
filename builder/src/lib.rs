f4dbc7489605757f2bd921d869408af2c41e4441
Tetsushi Omi <tetsu3491@gmail.com>
2022-02-18 21:13:52 -0500

    works

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,6 +4,26 @@ use quote::{format_ident, quote};
 //use syn::{parse_macro_input, DeriveInput, Ident};
 use syn::{parse_macro_input, DeriveInput};
 
+fn ty_inner_type(ty: &syn::Type) -> Option<&syn::Type> {
+    if let syn::Type::Path(p) = ty {
+        if p.path.segments.len() != 1 || p.path.segments[0].ident != "Option" {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(ref inner_ty) = p.path.segments[0].arguments {
+            if inner_ty.args.len() != 1 {
+                return None;
+            }
+
+            let inner_ty = inner_ty.args.pairs().next().unwrap();
+            if let syn::GenericArgument::Type(t) = inner_ty.value() {
+                return Some(t);
+            }
+        }
+    }
+    None
+}
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
@@ -26,22 +46,42 @@ pub fn derive(input: TokenStream) -> TokenStream {
         let name = &f.ident;
         let ty = &f.ty;
         //quote! { #name: std::option::Option<#ty> }
-        quote! { #name: Option<#ty> }
+        if ty_inner_type(ty).is_some() {
+            quote! { #name: #ty }
+        } else {
+            quote! { #name: Option<#ty> }
+        }
     });
     let methods = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        quote! {
-            fn #name(&mut self, #name: #ty) -> &mut Self {
-                self.#name = Some(#name);
-                self
+        if let Some(inner_ty) = ty_inner_type(ty) {
+            quote! {
+                pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
             }
         }
     });
     let build_fields = fields.iter().map(|f| {
         let name = &f.ident;
-        quote! {
-            #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+        let ty = &f.ty;
+        if ty_inner_type(ty).is_some() {
+            quote! {
+                #name: self.#name.clone()
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+            }
         }
     });
     let build_empty = fields.iter().map(|f| {
