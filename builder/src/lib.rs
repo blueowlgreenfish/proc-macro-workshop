67fabc5e727a5f95f12bacac668547f37bfb5e47
Tetsushi Omi <tetsu3491@gmail.com>
2022-02-21 16:17:53 -0500

    better

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -31,6 +31,38 @@ fn builder_of(f: &syn::Field) -> Option<proc_macro2::Group> {
     None
 }
 
+fn extend_method(f: &syn::Field) -> Option<(bool, proc_macro2::TokenStream)> {
+    let name = f.ident.as_ref().unwrap();
+    let g = builder_of(f)?;
+    let mut tokens = g.stream().into_iter();
+    match tokens.next().unwrap() {
+        TokenTree::Ident(ref i) => assert_eq!(i, "each"),
+        tt => panic!("expected 'each', found {}", tt),
+    }
+    match tokens.next().unwrap() {
+        TokenTree::Punct(ref p) => assert_eq!(p.as_char(), '='),
+        tt => panic!("expected '=', found {}", tt),
+    }
+    let arg = match tokens.next().unwrap() {
+        TokenTree::Literal(l) => l,
+        tt => panic!("expected string, found {}", tt),
+    };
+    match syn::Lit::new(arg) {
+        syn::Lit::Str(s) => {
+            let arg = syn::Ident::new(&s.value(), s.span());
+            let inner_ty = ty_inner_type("Vec", &f.ty).unwrap();
+            let method = quote! {
+                pub fn #arg(&mut self, #arg: #inner_ty) -> &mut Self {
+                    self.#name.push(#arg);
+                    self
+                }
+            };
+            Some((&arg == name, method))
+        },
+        lit => panic!("expected string, found {:?}", lit),
+    }
+}
+
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
@@ -56,7 +88,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     });
     let methods = fields.iter().map(|f| {
-        let name = &f.ident;
+        let name = f.ident.as_ref().unwrap();
         let ty = &f.ty;
         let set_method = if let Some(inner_ty) = ty_inner_type("Option", ty) {
             quote! {
@@ -139,36 +171,4 @@ pub fn derive(input: TokenStream) -> TokenStream {
     };
 
     expanded.into()
-}
-
-fn extend_method(f: &syn::Field) -> Option<(bool, proc_macro2::TokenStream)> {
-    let name = f.ident.as_ref().unwrap();
-    let g = builder_of(f)?;
-    let mut tokens = g.stream().into_iter();
-    match tokens.next().unwrap() {
-        TokenTree::Ident(ref i) => assert_eq!(i, "each"),
-        tt => panic!("expected 'each', found {}", tt),
-    }
-    match tokens.next().unwrap() {
-        TokenTree::Punct(ref p) => assert_eq!(p.as_char(), '='),
-        tt => panic!("expected '=', found {}", tt),
-    }
-    let arg = match tokens.next().unwrap() {
-        TokenTree::Literal(l) => l,
-        tt => panic!("expected string, found {}", tt),
-    };
-    match syn::Lit::new(arg) {
-        syn::Lit::Str(s) => {
-            let arg = syn::Ident::new(&s.value(), s.span());
-            let inner_ty = ty_inner_type("Vec", &f.ty).unwrap();
-            let method = quote! {
-                pub fn #arg(&mut self, #arg: #inner_ty) -> &mut Self {
-                    self.#name.push(#arg);
-                    self
-                }
-            };
-            Some((&arg == name, method))
-        },
-        lit => panic!("expected string, found {:?}", lit),
-    }
 }
