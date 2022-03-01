314113c4009bbbb244751d2932af8cd6d0be08f7
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-01 15:50:03 -0500

    ha

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,31 @@
 use proc_macro::TokenStream;
+use syn::{
+    parse::{Parse, ParseStream, Result},
+    parse_macro_input, Token,
+};
+
+#[derive(Debug)]
+struct SeqMacroInput {/* ... */}
+
+impl Parse for SeqMacroInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let _var = syn::Ident::parse(input)?;
+        let _in = <Token![in]>::parse(input)?;
+        let _from = syn::LitInt::parse(input)?;
+        let _dots = <Token![..]>::parse(input)?;
+        let _to = syn::LitInt::parse(input)?;
+        let _body = syn::Block::parse(input)?;
+        // println!("{:#?}", to);
+        // eprintln!("{:?} {:?} {:?} {:?} {:?}", var, _in, from, _dots, to);
+        // eprintln!("{:?}", body);
+        // eprintln!("{:#?}", var);
+        Ok(SeqMacroInput {})
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _input = parse_macro_input!(input as SeqMacroInput);
+    // println!("{:#?}", input);
+    TokenStream::new()
 }
