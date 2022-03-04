b4a3f998e95c8063a133a10047c5cc49613262f4
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-04 06:13:11 -0500

    cleanup

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -80,26 +80,19 @@ impl SeqMacroInput {
             },
             proc_macro2::TokenTree::Ident(mut ident) => {
                 let mut peek = rest.clone();
-                match peek.next() {
-                    Some(proc_macro2::TokenTree::Punct(ref punct)) if punct.as_char() == '~' => {
-                        match peek.next() {
-                            Some(proc_macro2::TokenTree::Ident(ref ident2))
-                                if ident2 == &self.ident =>
-                            {
-                                ident = proc_macro2::Ident::new(
-                                    &format!("{}{}", ident, i),
-                                    ident.span(),
-                                );
-                                *rest = peek.clone();
+                match (peek.next(), peek.next()) {
+                    (
+                        Some(proc_macro2::TokenTree::Punct(ref punct)),
+                        Some(proc_macro2::TokenTree::Ident(ref ident2)),
+                    ) if punct.as_char() == '~' && ident2 == &self.ident => {
+                        ident = proc_macro2::Ident::new(&format!("{}{}", ident, i), ident.span());
+                        *rest = peek.clone();
 
-                                match peek.next() {
-                                    Some(proc_macro2::TokenTree::Punct(ref punct))
-                                        if punct.as_char() == '~' =>
-                                    {
-                                        *rest = peek.clone();
-                                    },
-                                    _ => {},
-                                }
+                        match peek.next() {
+                            Some(proc_macro2::TokenTree::Punct(ref punct))
+                                if punct.as_char() == '~' =>
+                            {
+                                *rest = peek.clone();
                             },
                             _ => {},
                         }
