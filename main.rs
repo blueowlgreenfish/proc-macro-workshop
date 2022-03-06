f0a2b354a298483c5a7c64b717512b35ee93cb04
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-06 16:15:52 -0500

    start

--- main.rs
+++ main.rs
@@ -1,24 +1,22 @@
-// At this point we have an enum and we need to check whether the variants
-// appear in sorted order!
-//
-// When your implementation notices a variant that compares lexicographically
-// less than one of the earlier variants, you'll want to construct a syn::Error
-// that gets converted to TokenStream by the already existing code that handled
-// this conversion during the previous test case.
-//
-// The "span" of your error, which determines where the compiler places the
-// resulting error message, will be the span of whichever variant name that is
-// not in the right place. Ideally the error message should also identify which
-// other variant the user needs to move this one to be in front of.
+// This test is similar to the previous where want to ensure that the macro
+// correctly generates an error when the input enum is out of order, but this
+// time it is using an enum that also has data associated with each variant.
 
 use sorted::sorted;
 
+use std::env::VarError;
+use std::error::Error as StdError;
+use std::fmt;
+use std::io;
+use std::str::Utf8Error;
+
 #[sorted]
 pub enum Error {
-    ThatFailed,
-    ThisFailed,
-    SomethingFailed,
-    WhoKnowsWhatFailed,
+    Fmt(fmt::Error),
+    Io(io::Error),
+    Utf8(Utf8Error),
+    Var(VarError),
+    Dyn(Box<dyn StdError>),
 }
 
 fn main() {}
