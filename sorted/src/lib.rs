e4d2cec864c7415d9dc59811449a7bc25ada72ad
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-06 05:16:28 -0500

    works

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -4,10 +4,19 @@ use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    // let ty = dbg!(parse_macro_input!(input as syn::ItemEnum));
-    let ty = parse_macro_input!(input as syn::ItemEnum);
+    let ty = parse_macro_input!(input as syn::Item);
     println!("{:#?}", ty);
     assert!(args.is_empty());
-    let ts = quote! { #ty };
-    ts.into()
+
+    sorted_impl(ty).into()
+}
+
+fn sorted_impl(input: syn::Item) -> proc_macro2::TokenStream {
+    if let syn::Item::Enum(e) = input {
+        quote! { #e }
+    } else {
+        quote! {
+            compile_error!("expected enum or match expression");
+        }
+    }
 }
