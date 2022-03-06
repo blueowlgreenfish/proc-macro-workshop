9ae17cdff752b1f2cbb8de63753c61d895b8f00d
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-06 04:23:50 -0500

    works

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,13 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
-
-    unimplemented!()
+    // let ty = dbg!(parse_macro_input!(input as syn::ItemEnum));
+    let ty = parse_macro_input!(input as syn::ItemEnum);
+    println!("{:#?}", ty);
+    assert!(args.is_empty());
+    let ts = quote! { #ty };
+    ts.into()
 }
