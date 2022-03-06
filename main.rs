64334868700f9692cc756f452e2cd88fbd7478d7
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-06 03:04:46 -0500

    start

--- main.rs
+++ main.rs
@@ -1,63 +1,42 @@
-// Suppose we wanted a seq invocation in which the upper bound is given by the
-// value of a const. Both macros and consts are compile-time things so this
-// seems like it should be easy.
+// This test checks that an attribute macro #[sorted] exists and is imported
+// correctly in the module system. If you make the macro return an empty token
+// stream or exactly the original token stream, this test will already pass!
 //
-//     static PROCS: [Proc; NPROC] = seq!(N in 0..NPROC { ... });
+// Be aware that the meaning of the return value of an attribute macro is
+// slightly different from that of a derive macro. Derive macros are only
+// allowed to *add* code to the caller's crate. Thus, what they return is
+// compiled *in addition to* the struct/enum that is the macro input. On the
+// other hand attribute macros are allowed to add code to the caller's crate but
+// also modify or remove whatever input the attribute is on. The TokenStream
+// returned by the attribute macro completely replaces the input item.
 //
-// In fact it isn't, because macro expansion in Rust happens entirely before
-// name resolution. That is, a macro might know that something is called NPROC
-// but has no way to know which of many NPROC values in the dependency graph
-// this identifier refers to. Or maybe the NPROC constant doesn't exist yet when
-// the macro runs because it is only emitted by a later macro that hasn't run
-// yet. In this compilation model it isn't possible to support a query API where
-// a macro can give it the name of a constant and receive back the value of the
-// constant.
+// Before moving on to the next test, I recommend also parsing the input token
+// stream as a syn::Item. In order for Item to be available you will need to
+// enable `features = ["full"]` of your dependency on Syn, since by default Syn
+// only builds the minimum set of parsers needed for derive macros.
 //
-// All hope is not lost; it just means that our source of truth for the value of
-// NPROC must be a macro rather than a constant. The code in this test case
-// implements this workaround.
+// After parsing, the macro can return back exactly the original token stream so
+// that the input enum remains in the callers code and continues to be usable by
+// code in the rest of the crate.
 //
-// This test case may or may not require code changes in your seq macro
-// implementation depending on how you have implemented it so far. Before
-// jumping into any code changes, make sure you understand what the code in this
-// test case is trying to do.
+//
+// Resources:
+//
+//   - The Syn crate for parsing procedural macro input:
+//     https://github.com/dtolnay/syn
+//
+//   - The syn::Item type which represents a parsed enum as a syntax tree:
+//     https://docs.rs/syn/1.0/syn/enum.Item.html
 
-use seq::seq;
+use sorted::sorted;
 
-// Source of truth. Call a given macro passing nproc as argument.
-//
-// We want this number to appear in only one place so that updating this one
-// number will correctly affect anything that depends on the number of procs.
-macro_rules! pass_nproc {
-    ($mac:ident) => {
-        $mac! { 256 }
-    };
+#[sorted]
+pub enum Conference {
+    RustBeltRust,
+    RustConf,
+    RustFest,
+    RustLatam,
+    RustRush,
 }
-
-macro_rules! literal_identity_macro {
-    ($nproc:literal) => {
-        $nproc
-    };
-}
-
-// Expands to: `const NPROC: usize = 256;`
-const NPROC: usize = pass_nproc!(literal_identity_macro);
-
-struct Proc;
-
-impl Proc {
-    const fn new() -> Self {
-        Proc
-    }
-}
-
-macro_rules! make_procs_array {
-    ($nproc:literal) => {
-        seq!(N in 0..$nproc { [#(Proc::new(),)*] })
-    }
-}
-
-// Expands to: `static PROCS: [Proc; NPROC] = [Proc::new(), ..., Proc::new()];`
-static PROCS: [Proc; NPROC] = pass_nproc!(make_procs_array);
 
 fn main() {}
