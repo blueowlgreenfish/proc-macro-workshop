05200098f788a5e501bb4e36046392af1bd793f3
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-06 05:02:41 -0500

    cleanup

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -5,7 +5,7 @@ use syn::parse_macro_input;
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     // let ty = dbg!(parse_macro_input!(input as syn::ItemEnum));
-    let ty = parse_macro_input!(input as syn::ItemEnum);
+    let ty = parse_macro_input!(input as syn::Item);
     println!("{:#?}", ty);
     assert!(args.is_empty());
     let ts = quote! { #ty };
