5d4e0b3274672ee0920eae46ea15563679789417
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-10 18:05:43 -0500

    start

--- main.rs
+++ main.rs
@@ -1,18 +1,29 @@
-// The macro won't need to define what it means for other sorts of patterns to
-// be sorted. It should be fine to trigger an error if any of the patterns is
-// not something that can be compared by path.
-//
-// Be sure that the resulting error message is understandable and placed
-// correctly underlining the unsupported pattern.
+// There is one other common type of pattern that would be nice to support --
+// the wildcard or underscore pattern. The #[sorted] macro should check that if
+// a wildcard pattern is present then it is the last one.
 
-#[sorted::check]
-fn f(bytes: &[u8]) -> Option<u8> {
-    #[sorted]
-    match bytes {
-        [] => Some(0),
-        [a] => Some(*a),
-        [a, b] => Some(a + b),
-        _other => None,
+use sorted::sorted;
+
+#[sorted]
+pub enum Conference {
+    RustBeltRust,
+    RustConf,
+    RustFest,
+    RustLatam,
+    RustRush,
+}
+
+impl Conference {
+    #[sorted::check]
+    pub fn region(&self) -> &str {
+        use self::Conference::*;
+
+        #[sorted]
+        match self {
+            RustFest => "Europe",
+            RustLatam => "Latin America",
+            _ => "elsewhere",
+        }
     }
 }
 
