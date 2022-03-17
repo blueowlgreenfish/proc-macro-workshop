5600062da49eaca532c9d8adbc6bd072320fa639
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-17 19:11:09 -0400

    cleanup

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -7,10 +7,11 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     // println!("{:#?}", input);
     let ident = input.ident;
+    let ident_string = ident.to_string();
     let expand = quote! {
         impl std::fmt::Debug for #ident {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                f.debug_struct("Field")
+                f.debug_struct(#ident_string)
                     .field("name", &self.name)
                     .field("bitmask", &self.bitmask)
                     .finish()
