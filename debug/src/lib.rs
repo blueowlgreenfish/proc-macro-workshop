18e12f08058e3bc336914743ab96cca64d8e30b1
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-19 06:56:23 -0400

    tmp

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     // println!("{:#?}", input);
@@ -18,8 +18,39 @@ pub fn derive(input: TokenStream) -> TokenStream {
         unimplemented!();
     };
     let smaller_expand = data.into_iter().map(|f| {
+        let mut lit = String::new();
+        if !f.attrs.is_empty() {
+            for attr in f.attrs {
+                if let Ok(syn::Meta::NameValue(hi)) = attr.parse_meta() {
+                    match hi.lit {
+                        syn::Lit::Str(yes) => {
+                            lit = yes.value();
+                        },
+                        _ => {
+                            unimplemented!();
+                        },
+                    }
+                    // lit = hi.lit
+                }
+                // if let Ok(meta_value) = attr.parse_meta() {
+                //     if let syn::Meta::NameValue(hi) = meta_value {
+                //         lit_value = hi.lit;
+                //     }
+                // }
+            }
+        };
+        // let lit_string = match lit {
+        //     syn::Lit::Str(yes) => yes.value(),
+        //     _ => {
+        //         unimplemented!();
+        //     },
+        // };
         let field_ident = f.ident.as_ref().unwrap();
         let field_ident_string = field_ident.to_string();
+        if !lit.is_empty() {
+            println!("yo{}", lit);
+        }
+        //let debug = format!("{:?}", format_args!("{} foo {:?}", 1, 2));
         quote! {
             .field(#field_ident_string, &self.#field_ident)
         }
