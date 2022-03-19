225bbbb17670d3423dafa9e72760f72c7306e6fa
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-19 03:12:12 -0400

    better

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -8,12 +8,30 @@ pub fn derive(input: TokenStream) -> TokenStream {
     // println!("{:#?}", input);
     let ident = input.ident;
     let ident_string = ident.to_string();
+    let data = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+        ..
+    }) = input.data
+    {
+        named
+    } else {
+        unimplemented!();
+    };
+    let mut field_idents = Vec::new();
+    for value in data.into_iter() {
+        field_idents.push(value.ident.unwrap());
+    }
+    let smaller_expand = field_idents.iter().map(|f| {
+        let field_idents_string = f.to_string();
+        quote! { .field(#field_idents_string, &self.#f) }
+    });
     let expand = quote! {
         impl std::fmt::Debug for #ident {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 f.debug_struct(#ident_string)
-                    .field("name", &self.name)
-                    .field("bitmask", &self.bitmask)
+                    #(
+                        #smaller_expand
+                    )*
                     .finish()
             }
         }
