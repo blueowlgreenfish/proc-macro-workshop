a14efdf110c55516fa71d486b11796569d1c4822
Tetsushi Omi <tetsu3491@gmail.com>
2022-03-19 20:30:12 -0400

    works

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -30,29 +30,20 @@ pub fn derive(input: TokenStream) -> TokenStream {
                             unimplemented!();
                         },
                     }
-                    // lit = hi.lit
                 }
-                // if let Ok(meta_value) = attr.parse_meta() {
-                //     if let syn::Meta::NameValue(hi) = meta_value {
-                //         lit_value = hi.lit;
-                //     }
-                // }
             }
         };
-        // let lit_string = match lit {
-        //     syn::Lit::Str(yes) => yes.value(),
-        //     _ => {
-        //         unimplemented!();
-        //     },
-        // };
         let field_ident = f.ident.as_ref().unwrap();
         let field_ident_string = field_ident.to_string();
         if !lit.is_empty() {
-            println!("yo{}", lit);
-        }
-        //let debug = format!("{:?}", format_args!("{} foo {:?}", 1, 2));
-        quote! {
-            .field(#field_ident_string, &self.#field_ident)
+            quote! {
+                // & needed in front of format_args! to retain binary form
+                .field(#field_ident_string, &format_args!(#lit, &self.#field_ident))
+            }
+        } else {
+            quote! {
+                .field(#field_ident_string, &self.#field_ident)
+            }
         }
     });
     let expand = quote! {
